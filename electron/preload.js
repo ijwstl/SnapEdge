7252c0ba777e2d63969ed76620bcf675cc5770f7
ijwstl <asiaw159753@outlook.com>
2024-11-08 00:56:23 +0800

    删除electron文件

--- electron/preload.js
+++
@@ -1,7 +0,0 @@
-// preload.js
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  selectImageFile: () => ipcRenderer.invoke("select-image-file"),
-  readImageFile: (filePath) => ipcRenderer.invoke("read-image-file", filePath),
-});
